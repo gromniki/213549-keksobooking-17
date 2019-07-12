18137a0ae7bed0f983088bec3f5022104015f509
Roman Golubev <gromniki@gmail.com>
2019-07-13 00:04:54 +0400

    Правки предзащиты v2

--- js/backend.js
+++ js/backend.js
@@ -1,9 +1,9 @@
 'use strict';
 
 (function () {
-  var URL_DATA = {
-    load: 'https://js.dump.academy/keksobooking/data',
-    save: 'https://js.dump.academy/keksobooking'
+  var UrlData = {
+    LOAD: 'https://js.dump.academy/keksobooking/data',
+    SAVE: 'https://js.dump.academy/keksobooking'
   };
 
   var REQUEST_TIMEOUT = 10000; // 10s
@@ -39,13 +39,13 @@
 
   var load = function (onLoad, onError) {
     var xhr = createRequest(onLoad, onError);
-    xhr.open('GET', URL_DATA.load);
+    xhr.open('GET', UrlData.LOAD);
     xhr.send();
   };
 
   var save = function (data, onLoad, onError) {
     var xhr = createRequest(onLoad, onError);
-    xhr.open('POST', URL_DATA.save);
+    xhr.open('POST', UrlData.SAVE);
     xhr.send(data);
   };
 
