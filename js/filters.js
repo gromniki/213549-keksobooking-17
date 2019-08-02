7553ee63ea9cdd48e5ea62471950de81d9b056d2
Roman Golubev <gromniki@gmail.com>
2019-08-02 23:43:38 +0300

    Добавил модуль filters

---
+++ js/filters.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  var mainFormFilters = document.querySelector('.map__filters-container');
+  // var housingTypeFilter = mainFormFilters.querySelector('#housing-type');
+  var pricesFilter = mainFormFilters.querySelector('#housing-price');
+  var roomsFilter = mainFormFilters.querySelector('#housing-rooms');
+  var guestsFilter = mainFormFilters.querySelector('#housing-guests');
+  var featuresFilter = mainFormFilters.querySelector('#housing-features');
+
+  window.filters = {
+  };
+})();
